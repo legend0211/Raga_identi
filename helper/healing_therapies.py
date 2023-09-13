400776c6b5b46e095f687634ff8fc161f490f74d
Rohan <nickeyjha2@gmail.com>
2023-09-13 20:51:51 +0530

    optimized code

--- helper/healing_therapies.py
+++ helper/healing_therapies.py
@@ -1,50 +1,36 @@
 
 
 def healing_therapies(thaat_list):
-    thaat = ["Bilawal", "Kalyan", "Khamaj", "Kafi", "Asavari", "Todi", "Poorvi", "Marva", "Bhairavi", "Bhairav"]
+    info={'Bilawal': ['Gives healthy mind and body', 'Control sound and sonorous sleep'], 
+          'Kalyan': ['Reduce mental tension', 'Relief from headache', 
+                     'Relief from cough and cold', 'Relief from problems of high blood pressure', 
+                     'Can cure Rheumatic Arthritis'], 
+          'Khamaj': ['Control sound and sonorous sleep', 'Reduce mental tension', 
+                     'Relief from asthma', 'Prevents hysteria'], 
+          'Kafi': ['Gives healthy mind and body', 'Brings joy', 'Mitigate insomina', 
+                   'Reduces anxiety', 'Reduces hypertension'], 
+          'Asavari': ['Reduce mental tension', 'Reduces hypertension', 
+                      'Brings Creativity and Happiness', 'Cure low blood pressure', 
+                      'Control psychological hazards and builds confidence', 'Used for constipation'],  
+          'Todi': ['Reduce mental tension', 'Relief from headache', 'Relief from cough and cold', 
+                   'Prevents hysteria', 'Reduces anxiety', 'Brings serenity'], 
+          'Poorvi': ['Reduce mental tension', 'Mitigate insomina'],  
+          'Marva': ['Gives healthy mind and body', 'Prevents hysteria', 
+                    'Enhances compassion and patience'], 
+          'Bhairavi': ['Relief from headache', 'Brings Creativity and Happiness', 
+                       'Enhances compassion and patience', 'Relief from High fever', 
+                       'Relief from phlegm, toothache, intestinal gas, sinusitis'], 
+          'Bhairav': ['Relief from headache', 'Relief from cough and cold', 'Brings serenity', 
+                      'It can be used for Emotional strength, Devotion and Peace, Restful Sleep, Tranquility, Relaxation & Rest']}
     
-    therapy = ["Gives healthy mind and body",
-               "Control sound and sonorous sleep",
-               "Reduce mental tension",
-               "Relief from headache",
-               "Relief from cough and cold",
-               "Relief from problems of high blood pressure",
-               "Can cure Rheumatic Arthritis",
-               "Relief from asthma",
-               "Prevents hysteria",
-               "Brings joy",
-               "Mitigate insomina",
-               "Reduces anxiety",
-               "Reduces hypertension",
-               "Brings Creativity and Happiness",
-               "Cure low blood pressure",
-               "Control psychological hazards and builds confidence",
-               "Used for constipation",
-               "Brings serenity",
-               "Enhances compassion and patience",
-               "Relief from High fever",
-               "Relief from phlegm, toothache, intestinal gas, sinusitis",
-               "It can be used for Emotional strength, Devotion and Peace, Restful Sleep, Tranquility, Relaxation & Rest",
-               "Peace Integration"]
-    
-    therapy_choice = [[1,2],
-                      [3,4,5,6,7],
-                      [2,3,8,9],
-                      [1,10,11,12,13],
-                      [3,13,14,15,16,17],
-                      [3,4,5,9,12,18],
-                      [3,11],
-                      [0,9,19],
-                      [4,14,19,20,21],
-                      [4,5,18,22]]
     
     therapy_number = set()
-    for i in range(len(thaat)):
-        if(thaat[i] in thaat_list):
-            for j in therapy_choice[i]:
-                    therapy_number.add(therapy[j])
+    
+    for thaat in thaat_list:
+        for t in info[thaat]:
+            therapy_number.add(t)
     
    
     return list(therapy_number)
 
-print(healing_therapies(["Bilawal"]))
+print(healing_therapies(["Bilawal","Poorvi","Marva","Asavari"]))#test
--- helper/playing_time.py
+++ helper/playing_time.py
@@ -1,19 +1,23 @@
 
 def playing_time(thaat_list):
-  thaat = ["Bilawal", "Kalyan", "Khamaj", "Kafi", "Asavari", "Todi", "Poorvi", "Marva", "Bhairavi", "Bhairav"]
-
-  playing_time=["Anytime","Morining","Afternoon","Evening","Night"]
-
-  possible_choice=[[0],[0],[3,4],[1],[1,2,3],[1,3],[1],[1,4],[0],[1,2]]
-
+  info={"Bilwal":["Anytime"],
+        "Kalyan":["Anytime"],
+        "Khamaj":["Afternoon","Evening"],
+        "Kafi":["Anytime"],
+        "Asavari":["Morning","Afternoon","Evening"],
+        "Todi":["Morning","Evening"],
+        "Poorvi":["Anytime"],
+        "Marva":["Morning","Night"],
+        "Bhairavi":["Anytime"],
+        "Bhairav":["Morning","Afternoon"],}
+  
   time=set()
 
-  for i in range(len(thaat)):
-    if thaat[i] in thaat_list:
-      for j in possible_choice[i]:
-        time.add(playing_time[j])
+  for thaat in thaat_list:
+    for t in info[thaat]:
+      time.add(t)
 
   return list(time)
 
 
-print(playing_time(["Bhairavi","Kalyan"]))
+print(playing_time(["Marva","Bhairav"]))#test
